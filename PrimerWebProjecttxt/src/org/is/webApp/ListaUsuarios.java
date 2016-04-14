83d63fe8e6a91c22d5ac3f45a840b8c176135633
Aula8 <Aula8@A_08_07.aulastic2008.ual.es>
2016-04-14 20:25:27 +0200

    cambio de ruta del archivo

--- PrimerWebProjecttxt/src/org/is/webApp/ListaUsuarios.java
+++ PrimerWebProjecttxt/src/org/is/webApp/ListaUsuarios.java
@@ -14,7 +14,8 @@ public class ListaUsuarios {
 	// private String rutaArchivo = "PrimerWebProject/WebContent/datos/usuarios.txt";
 
 	// deploy on Ubuntu
-	private String rutaArchivo = "PrimerWebProjecttxt/WebContent/datos/usuarios.txt";
+	//private String rutaArchivo = "PrimerWebProjecttxt/WebContent/datos/usuarios.txt";
+	private String rutaArchivo = "$CATALINA_HOME/webapps/PrimerWebProject-0.0.1-manale/datos/usuarios.txt";
 	
 	/* TIP: Edit tomcat defaul directory:
 	*  and I edit the tomcat argument in Eclipse IDE. 
